9ae8d1a7831d208d7acaa79629cd6e0783096566
sarit <sarit@DESKTOP-KSJF0BF>
2019-11-23 13:53:28 -0400

    Test

---
+++ mvcPhone/apiPhoneTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("apiPhoneTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("apiPhoneTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e83b7edb-71a5-4473-a915-03abb150f3dd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ mvcPhone/apiPhoneTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace apiPhoneTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1Exist()
+        { 
+            //organizar
+            var Controler = new apiPhone.Controllers.PhonesController();
+
+            //actuar
+            var response = Controler.PhoneExists(1);
+
+            //afirmar
+            Assert.AreEqual(Controler,response);
+        }
+    }
+}
